8d6685a8bf03aae07957e62a9c922731b7e3a490
bariskarapinar <bkarapinar1@hotmail.com>
2026-08-08 12:04:21 +0300

    Filters.kt Update

---
+++ app/src/main/java/com/myapp/jetsnack/ui/components/Filters.kt
@@ -0,0 +1,143 @@
+@file:OptIn(ExperimentalSharedTransitionApi::class)
+
+package com.myapp.jetsnack.ui.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FilterList
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.dp
+import com.myapp.jetsnack.model.Filter
+import com.myapp.jetsnack.ui.theme.JetsnackTheme
+
+@Composable
+fun FilterBar(
+    filters: List<Filter>,
+    onShowFilters: () -> Unit,
+    filterScreenVisible: Boolean,
+    sharedTransitionScope: SharedTransitionScope,
+) {
+    with(sharedTransitionScope) {
+        LazyRow(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = PaddingValues(start = 12.dp, end = 8.dp),
+            modifier = Modifier.heightIn(min = 56.dp),
+        ) {
+            item {
+                AnimatedVisibility(visible = !filterScreenVisible) {
+                    IconButton(
+                        onClick = onShowFilters,
+                        modifier = Modifier
+                            .sharedBounds(
+                                rememberSharedContentState("FilterSharedElementKey"),
+                                animatedVisibilityScope = this@AnimatedVisibility,
+                                resizeMode = SharedTransitionScope.ResizeMode.RemeasureToBounds,
+                            ),
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.FilterList,
+                            tint = JetsnackTheme.colors.brand,
+                            contentDescription = "Filters",
+                            modifier = Modifier.diagonalGradientBorder(
+                                colors = JetsnackTheme.colors.interactiveSecondary,
+                                shape = CircleShape,
+                            ),
+                        )
+                    }
+                }
+            }
+            items(filters) { filter ->
+                FilterChip(filter = filter, shape = MaterialTheme.shapes.small)
+            }
+        }
+    }
+}
+
+@Composable
+fun FilterChip(
+    filter: Filter,
+    modifier: Modifier = Modifier,
+    shape: Shape = MaterialTheme.shapes.small
+) {
+    val (selected, setSelected) = filter.enabled
+    val backgroundColor by animateColorAsState(
+        if (selected) JetsnackTheme.colors.brandSecondary else JetsnackTheme.colors.uiBackground,
+        label = "background color",
+    )
+    val border = Modifier.fadeInDiagonalGradientBorder(
+        showBorder = !selected,
+        colors = JetsnackTheme.colors.interactiveSecondary,
+        shape = shape,
+    )
+    val textColor by animateColorAsState(
+        if (selected) Color.Black else JetsnackTheme.colors.textSecondary,
+        label = "text color",
+    )
+
+    JetsnackSurface(
+        modifier = modifier,
+        color = backgroundColor,
+        contentColor = textColor,
+        shape = shape,
+        elevation = 2.dp,
+    ) {
+        val interactionSource = remember { MutableInteractionSource() }
+        val pressed by interactionSource.collectIsPressedAsState()
+        val backgroundPressed = if (pressed) {
+            Modifier.offsetGradientBackground(
+                JetsnackTheme.colors.interactiveSecondary,
+                200f,
+                0f,
+            )
+        } else {
+            Modifier.background(Color.Transparent)
+        }
+        Box(
+            modifier = Modifier
+                .toggleable(
+                    value = selected,
+                    onValueChange = setSelected,
+                    interactionSource = interactionSource,
+                    indication = null,
+                )
+                .then(backgroundPressed)
+                .then(border),
+        ) {
+            Text(
+                text = filter.name,
+                style = MaterialTheme.typography.bodySmall,
+                maxLines = 1,
+                modifier = Modifier.padding(
+                    horizontal = 20.dp,
+                    vertical = 6.dp,
+                ),
+            )
+        }
+    }
+}
