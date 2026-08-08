351a5c8267edacf3c25650965136a8fefdbc183e
bariskarapinar <bkarapinar1@hotmail.com>
2026-08-08 12:04:38 +0300

    Feed.kt Update

---
+++ app/src/main/java/com/myapp/jetsnack/ui/home/Feed.kt
@@ -0,0 +1,122 @@
+@file:OptIn(ExperimentalSharedTransitionApi::class)
+
+package com.myapp.jetsnack.ui.home
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionLayout
+import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.add
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsTopHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.myapp.jetsnack.model.Filter
+import com.myapp.jetsnack.model.SnackCollection
+import com.myapp.jetsnack.model.SnackRepo
+import com.myapp.jetsnack.ui.components.FilterBar
+import com.myapp.jetsnack.ui.components.JetsnackDivider
+import com.myapp.jetsnack.ui.components.JetsnackSurface
+import com.myapp.jetsnack.ui.components.SnackCollection
+
+@Composable
+fun Feed(
+    onSnackClick: (Long, String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val snackCollections = remember { SnackRepo.getSnacks() }
+    val filters = remember { SnackRepo.getFilters() }
+    Feed(
+        snackCollections,
+        filters,
+        onSnackClick,
+        modifier,
+    )
+}
+
+@Composable
+private fun Feed(
+    snackCollections: List<SnackCollection>,
+    filters: List<Filter>,
+    onSnackClick: (Long, String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    JetsnackSurface(modifier = modifier.fillMaxSize()) {
+        var filtersVisible by remember { mutableStateOf(false) }
+        SharedTransitionLayout {
+            Box {
+                SnackCollectionList(
+                    snackCollections,
+                    filters,
+                    filtersVisible = filtersVisible,
+                    onFiltersSelected = { filtersVisible = true },
+                    sharedTransitionScope = this@SharedTransitionLayout,
+                    onSnackClick = onSnackClick,
+                )
+                DestinationBar()
+                AnimatedVisibility(
+                    filtersVisible,
+                    enter = fadeIn(),
+                    exit = fadeOut()
+                ) {
+                    FilterScreen(
+                        animatedVisibilityScope = this@AnimatedVisibility,
+                        sharedTransitionScope = this@SharedTransitionLayout,
+                    ) {
+                        filtersVisible = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun SnackCollectionList(
+    snackCollections: List<SnackCollection>,
+    filters: List<Filter>,
+    filtersVisible: Boolean,
+    onFiltersSelected: () -> Unit,
+    onSnackClick: (Long, String) -> Unit,
+    sharedTransitionScope: SharedTransitionScope,
+    modifier: Modifier = Modifier,
+) {
+    LazyColumn(modifier = modifier) {
+        item {
+            Spacer(
+                Modifier.windowInsetsTopHeight(
+                    WindowInsets.statusBars.add(WindowInsets(top = 56.dp)),
+                ),
+            )
+            FilterBar(
+                filters,
+                sharedTransitionScope = sharedTransitionScope,
+                filterScreenVisible = filtersVisible,
+                onShowFilters = onFiltersSelected,
+            )
+        }
+        itemsIndexed(snackCollections) { index, snackCollection ->
+            if (index > 0) {
+                JetsnackDivider(thickness = 2.dp)
+            }
+            SnackCollection(
+                snackCollection = snackCollection,
+                onSnackClick = onSnackClick,
+                index = index,
+            )
+        }
+    }
+}
