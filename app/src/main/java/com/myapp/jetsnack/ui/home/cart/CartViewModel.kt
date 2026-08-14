eb402d256e3f91c1570e5e5ac3a90323594c460b
bariskarapinar <bkarapinar1@hotmail.com>
2026-08-14 10:34:25 +0300

    Cart MVVM Update

--- app/src/main/java/com/myapp/jetsnack/ui/home/cart/CartViewModel.kt
+++ app/src/main/java/com/myapp/jetsnack/ui/home/cart/CartViewModel.kt
@@ -2,6 +2,7 @@ package com.myapp.jetsnack.ui.home.cart
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.myapp.jetsnack.model.CartRepo
 import com.myapp.jetsnack.model.OrderLine
 import com.myapp.jetsnack.model.SnackRepo
 import com.myapp.jetsnack.model.SnackbarManager
@@ -11,18 +12,20 @@ import kotlinx.coroutines.flow.StateFlow
 /**
  * Holds the contents of the cart and allows changes to it.
  */
-class CartViewModel(private val snackbarManager: SnackbarManager, snackRepository: SnackRepo) : ViewModel() {
+class CartViewModel(
+    private val snackbarManager: SnackbarManager,
+    private val cartRepo: CartRepo = CartRepo
+) : ViewModel() {
 
-    private val _orderLines: MutableStateFlow<List<OrderLine>> = MutableStateFlow(snackRepository.getCart())
-    val orderLines: StateFlow<List<OrderLine>> get() = _orderLines
+    val orderLines: StateFlow<List<OrderLine>> = cartRepo.orderLines
 
     private var requestCount = 0
     private fun shouldRandomlyFail(): Boolean = ++requestCount % 5 == 0
 
     fun increaseSnackCount(snackId: Long) {
         if (!shouldRandomlyFail()) {
-            val currentCount = _orderLines.value.first { it.snack.id == snackId }.count
-            updateSnackCount(snackId, currentCount + 1)
+            val currentCount = orderLines.value.first { it.snack.id == snackId }.count
+            cartRepo.updateSnackCount(snackId, currentCount + 1)
         } else {
             snackbarManager.showMessage("Could not increase count. Please try again.")
         }
@@ -30,11 +33,11 @@ class CartViewModel(private val snackbarManager: SnackbarManager, snackRepositor
 
     fun decreaseSnackCount(snackId: Long) {
         if (!shouldRandomlyFail()) {
-            val currentCount = _orderLines.value.first { it.snack.id == snackId }.count
+            val currentCount = orderLines.value.first { it.snack.id == snackId }.count
             if (currentCount == 1) {
                 removeSnack(snackId)
             } else {
-                updateSnackCount(snackId, currentCount - 1)
+                cartRepo.updateSnackCount(snackId, currentCount - 1)
             }
         } else {
             snackbarManager.showMessage("Could not decrease count. Please try again.")
@@ -42,27 +45,22 @@ class CartViewModel(private val snackbarManager: SnackbarManager, snackRepositor
     }
 
     fun removeSnack(snackId: Long) {
-        _orderLines.value = _orderLines.value.filter { it.snack.id != snackId }
+        cartRepo.removeSnack(snackId)
     }
 
-    private fun updateSnackCount(snackId: Long, count: Int) {
-        _orderLines.value = _orderLines.value.map {
-            if (it.snack.id == snackId) {
-                it.copy(count = count)
-            } else {
-                it
-            }
-        }
+    fun checkout() {
+        cartRepo.clearCart()
+        snackbarManager.showMessage("Ordered! Thank you for your purchase.")
     }
 
     companion object {
         fun provideFactory(
             snackbarManager: SnackbarManager = SnackbarManager,
-            snackRepository: SnackRepo = SnackRepo,
+            cartRepo: CartRepo = CartRepo,
         ): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
             @Suppress("UNCHECKED_CAST")
             override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                return CartViewModel(snackbarManager, snackRepository) as T
+                return CartViewModel(snackbarManager, cartRepo) as T
             }
         }
     }
